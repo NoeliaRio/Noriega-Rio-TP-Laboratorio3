8f943c40a754a5c7ef8497d1143976681994373a
Noelia <noe.lucia.rio@gmail.com>
2022-06-13 01:11:08 -0300

    Cambios en js calcular

--- Segundo-tp-lab3-Noriega-Río-3/script.js
+++ Segundo-tp-lab3-Noriega-Río-3/script.js
@@ -36,7 +36,7 @@ Vue.createApp({
             }
             else this.errorInvertir='Por favor ingrese un numero mayor a 1000';
             if (this.Comprobante===true && this.Comprobantemonto===true)
-            this.MontoFinal = parseFloat(this.Monto) + this.Monto * (this.Dias / 360) * intereses();   
+            this.MontoFinal = parseFloat(this.Monto) + this.Monto * (this.Dias / 360) * Intereses();   
         },
 
     
@@ -51,40 +51,3 @@ Vue.createApp({
 
 }).mount('#app')
 
-let btnCalcular = document.getElementById('btnCalcular');
-let btnReinvertir = document.getElementById('btnReinvertir');
-btnCalcular.addEventListener('click', () =>{MostrarInversion();})
-btnReinvertir.addEventListener('click', () =>{Reinvertir();})
-
-function MostrarCuadro(Montos)
-{
-    document.querySelector("Cuadro");
-    
-    let Periodo1 = document.querySelectorAll("Periodo1");
-    let Periodo2 = document.querySelectorAll("Periodo2");
-    let Periodo3 = document.querySelectorAll("Periodo3");
-    let Periodo4 = document.querySelectorAll("Periodo4");
-    
-    for (let i = 1; i < Periodo1.length; i++) 
-    {
-        Periodo1[i].textContent = "$ " + Montos[0][i - 1];
-        Periodo2[i].textContent = "$ " + Montos[1][i - 1];
-        Periodo3[i].textContent = "$ " + Montos[2][i - 1];
-        Periodo4[i].textContent = "$ " + Montos[3][i - 1];
-    }
-}
-function Reinvertir()
-{
-    let Monto = document.getElementById('MontoInvertido').value;
-    let Montos = [];
-    let MontoFinal= calcularMonto(Monto());
-    Montos.push([Monto, MontoFinal])
-    for (let i = 1; i < 4; i++) 
-    {
-        MontoFinal = calcularMonto(MontoFinal);
-        Montos.push([Montos[i - 1][1], MontoFinal])
-    }
-    MostrarDatosReinversion(Montos);
-}
-
-  const app = Vue.createApp({})
